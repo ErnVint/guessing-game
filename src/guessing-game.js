a7b12a96d45de2649d19f0b66e1c80a2eaf0d343
Ernest Yurkoits <ernvint@gmail.com>
2017-02-05 19:45:12 +0300

    100 passing

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,23 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        var _min, _max, guess;
+    }
 
     setRange(min, max) {
-
+        this._min = min;
+        this._max = max;
     }
 
     guess() {
-
+        return Math.ceil((this._min + this._max)/2);
     }
 
     lower() {
-
+        this._max = Math.ceil((this._min + this._max)/2);
     }
 
     greater() {
-
+        this._min = Math.ceil((this._min + this._max)/2);
     }
 }
 
